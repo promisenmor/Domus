3f06c5bd3643da488da06c3503c1e01babee4aa7
promisenmor <nmorchibogu98@gmail.com>
2025-05-16 23:13:37 +0100

    register models.py of contentapp on admin.py

--- domus/content/admin.py
+++ domus/content/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import  *
 
-# Register your models here.
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'published',)
+    list_filter = ('published', 'created_at',)
+
+
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'body',)
+
+@admin.register(MenuItem)
+class MenuItem(admin.ModelAdmin):
+    list_display = ('name', 'url', 'is_visible',)
+    list_filter = ('url', 'is_visible',)
