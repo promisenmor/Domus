709f5f44b776731d1cbd7f7f2804599b57640274
promisenmor <nmorchibogu98@gmail.com>
2025-05-16 22:57:21 +0100

    made migrations for content app

--- domus/content/models.py
+++ domus/content/models.py
@@ -17,3 +17,32 @@ class BlogPost(models.Model):
     
     def __str__(self):
         return self.title
+    
+
+class Page(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True, blank=True)
+    body = CKEditor5Field('body', config_name='default')
+    is_active = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+    
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    url = models.CharField(max_length=255)
+    order = models.PositiveIntegerField(default=0)
+    is_visible = models.BooleanField(default=True)
+
+
+    class Meta:
+        ordering = ['order']
+
+    def __str__(self):
+        return self.name
