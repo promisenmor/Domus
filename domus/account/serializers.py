2b1770aab532da741fc14e98609567139d8f5f74
promisenmor <nmorchibogu98@gmail.com>
2025-05-21 23:56:21 +0100

    made validations for user registrations

--- domus/account/serializers.py
+++ domus/account/serializers.py
@@ -5,19 +5,21 @@ User = get_user_model()
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True)
+    role = serializers.ChoiceField(choices=[
+        ('AGENT', 'Agent'),
+        ('CLIENT', 'Client'),
+    ], required=True)
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password', 'role', 'phone_number', 'company', 'license_number']
 
 
-        def validate_role(self, value):
-            if value == 'ADMIN':
-                raise serializers.ValidationError('You cannot register with ADMIN role')
-            return value
-        
-
         def create(self, validated_data):
+            role = validated_data('role')
+            if role == ['ADMIN', 'CLIENT']:
+                raise serializers.ValidationError("Invalid role for self-registration.")
+            
             password = validated_data.pop(password)
             user = User(**validated_data)
             user.set_password(password)
